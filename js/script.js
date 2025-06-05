7cd98e3ec5ffb715e0057d9457a3deb65adb01ff
Jhoni Davinne <jhoni.davinne@hotmail.com>
2025-06-05 02:11:20 -0300

    plugin animar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
-const links = document.querySelectorAll(".header-menu a");
-
-function ativarLink(link) {
+//Ativar links do menu
+document.querySelectorAll(".header-menu a").forEach((link) => {
     const url = location.href;
     const href = link.href;
     if (url.includes(href)) {
         link.classList.add("ativo");
     }
-}
+});
 
-links.forEach(ativarLink);
+//Ativar itens do orcamento
+new URLSearchParams(location.search).forEach((parametro) => {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+});
+
+//Perguntas frequentes
+document.querySelectorAll(".perguntas button").forEach((pergunta) => {
+    pergunta.addEventListener("click", (event) => {
+        const pergunta = event.currentTarget;
+        const controls = pergunta.getAttribute("aria-controls");
+        const resposta = document.getElementById(controls);
+
+        resposta.classList.toggle("ativo");
+        const ativa = resposta.classList.contains("ativo");
+        pergunta.setAttribute("aria-expanded", ativa);  
+    })
+});
+
+//Galeria de bicicletas
+document.querySelectorAll(".bicicleta-imagens img").forEach((img) => {
+    img.addEventListener("click", (event) => {
+        const img = event.currentTarget;
+        const media = matchMedia("(min-width: 1000px)").matches;
+        if (media) {
+            document.querySelector(".bicicleta-imagens").prepend(img);
+        }
+    });
+});
+
+//Animacao
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
